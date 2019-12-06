6b7ef2fed751c94e02d4dd09f38570755237bbf9
Shahin Pirooz <shahin@pirooz.net>
2019-12-06 07:49:27 -0800

    Update

--- DistilledSpirits.py
+++ DistilledSpirits.py
@@ -17,54 +17,55 @@
 #
 # -----------------------------------------------------------------------------
 # Imports
-import os, base64
+import os, requests, datetime
+from bs4 import BeautifulSoup
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import (
-        Mail, Attachment, FileContent, FileType,
-        FileName, Disposition, ContentId)
-import datetime
+from sendgrid.helpers.mail import Mail
 
 
 # -----------------------------------------------------------------------------
 # Variable Declarations
-sender = 'shahin@pirooz.net'
-receiver = 'shahin@emruz.com'
+sender = 'Shaq <shahin@pirooz.net>'
+receiver = ['shahin@emruz.com','scott@stephensongroup.net']
 subject = "Shaq's Distilled List - {}".format(datetime.date.today())
-html = """\
-<html>
-  <head></head>
-    <body>
-      <img src="cid:image1" alt="Logo" style="width:250px;height:50px;"><br>
-       <p><h4 style="font-size:15px;">Some Text.</h4></p>           
-    </body>
-</html>
-"""
-
-message = Mail(
-    from_email=sender,
-    to_emails=receiver,
-    subject=subject,
-    html_content=html)
-
-# The image file is in the same directory as the script
-fp = 'E.png'
-with open(fp, 'rb') as img:
-    data = img.read()
-    img.close()
-encoded = base64.b64encode(data).decode()
-attachment = Attachment()
-attachment.file_content = FileContent(encoded)
-attachment.file_type = FileType('application/png')
-attachment.file_name = FileName('logo.png')
-attachment.disposition = Disposition('attachment')
-attachment.content_id = ContentId('logo')
-message.attachment = attachment
-#print(message)
-
+url = "https://www.klwines.com/Products?&filters=sv2_dflt-stock-instock!30$eq$(216)$True$ff-30-(216)--$!28$eq$(3)$True$ff-28-(3)--$or,9.or,24.or,27.or,48!90$eq$1$True$ff-90-1--$or,61$eq$1$True$ff-61-1--$&orderBy=60%20asc,search.score()%20desc"
+apiKey = os.environ.get('SENDGRID_API_KEY', None)
 
 # =============================================================================
 # Functions
 # -----------------------------------------------------------------------------
+def GetDistilledList():
+    request = requests.get(url)
+    content = request.content
+    soup = BeautifulSoup(content, "html.parser")
+    element = soup.find("div", {"class": "results-block clearfix"})
+    
+    htmlHeader = """\
+    <html lang="en">
+       <head>
+          <meta charset="UTF-8">
+        <base href="https://www.klwines.com/" target="_blank">
+           <title>K&L's Distilled Spirits List</title>
+       </head>
+       <body>
+    """
+    
+    htmlFooter = """\
+    
+    </body>
+    </html>
+    
+    """
+
+    # -------------------------------------------------------------------------
+    # div = "<div class="results-block clearfix">"
+    request = requests.get(url)
+    content = request.content
+    soup = BeautifulSoup(content, "html.parser")
+    element = soup.find("div", {"class": "results-block clearfix"})
+
+    html = htmlHeader + str(element) + htmlFooter
+    return html
 
 
 # =============================================================================
@@ -72,14 +73,24 @@ message.attachment = attachment
 def main():
     # -------------------------------------------------------------------------
     # Send the message via SendGrid API.
-    try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-    except Exception as e:
-        print(e)
+    html = GetDistilledList()
+    message = Mail(
+        from_email=sender,
+        to_emails=receiver,
+        subject=subject,
+        html_content=html)
+
+    if apiKey is not None:
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e)
+    else:
+        print(f'The Sendgrid API key is: {apiKey}')
 
 if __name__ == '__main__':
     main() 
---
+++ GetDistilledList.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+"""
+@title: GetDistilledList.py
+@credits: Shahin Pirooz
+@company: Emruz
+@created: Mon Dec  2 16:01:13 2019
+@python: 3.6
+"""
+# =============================================================================
+""" This module... """
+# =============================================================================
+# Change History
+# Date        Notes
+# 20190312    Initial Version
+#
+# -----------------------------------------------------------------------------
+# Imports
+import requests
+from bs4 import BeautifulSoup
+
+# -----------------------------------------------------------------------------
+# Variable Declarations
+url = "https://www.klwines.com/Products?&filters=sv2_dflt-stock-instock!30$eq$(216)$True$ff-30-(216)--$!28$eq$(3)$True$ff-28-(3)--$or,9.or,24.or,27.or,48!90$eq$1$True$ff-90-1--$or,61$eq$1$True$ff-61-1--$&orderBy=60%20asc,search.score()%20desc"
+
+
+htmlHeader = """\
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+    <base href="https://www.klwines.com/" target="_blank">
+       <title>K&L's Distilled Spirits List</title>
+   </head>
+   <body>
+"""
+
+htmlFooter = """\
+
+</body>
+</html>
+
+"""
+
+
+
+# =============================================================================
+# Main Function
+def main():
+    # -------------------------------------------------------------------------
+    # div = "<div class="results-block clearfix">"
+    request = requests.get(url)
+    content = request.content
+    soup = BeautifulSoup(content, "html.parser")
+    element = soup.find("div", {"class": "results-block clearfix"})
+
+    html = htmlHeader + str(element) + htmlFooter
+    print(html[:1000])
+
+if __name__ == '__main__':
+    main() 
