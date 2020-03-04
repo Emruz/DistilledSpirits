f4c4d7d4dd64b88858b0fdcc013e629c0afbacf2
Shahin Pirooz <shahin@pirooz.net>
2020-03-03 23:26:24 -0800

    Update runArdbeg.cmd

--- runArdbeg.cmd
+++ runArdbeg.cmd
@@ -1,1 +1,1 @@
-c:\ProgramData\Anaconda3\python.exe C:\Users\Shahin Pirooz\Projects\DistilledSpirits\DistilledSpirits.Ardbeg.py
+c:\ProgramData\Anaconda3\python.exe "C:\Users\Shahin Pirooz\Projects\DistilledSpirits\DistilledSpirits.Ardbeg.py"
