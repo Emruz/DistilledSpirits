6ca98ffce7b7ca8f09b57c9f4a5b1df535bf6a65
Shahin Pirooz <shahinpirooz@users.noreply.github.com>
2021-08-12 13:43:48 -0700

    junk

--- getDistilledList.sh
+++ getDistilledList.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Users/shahin/opt/anaconda3/bin/python /Users/shahin/Projects/DistilledSpirits/DistilledSpirits.py
+/opt/anaconda3/bin/python /Users/shahin/Projects/DistilledSpirits/DistilledSpirits.py
